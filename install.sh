477713e832543f7a13f067b313a248d345503c16
Alejandro Blasco <wchmb@protonmail.com>
2017-05-27 19:44:16 +0200

    Update install.sh
    Download all configurations and package list examples

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/env sh
 
+echo
 echo 'Downloading files...'
-for f in iaa.sh iaa.conf pkglist.txt; do
+
+for f in iaa.sh iaa.conf iaa-i3.conf iaa-kde.conf pkglist.txt pkglist-gtk.txt pkglist-qt.txt; do
   echo $f
   curl -fOLs "https://raw.github.com/wachambo/install-arch-again/master/${f}"
   [ $? != '0' ] && echo 'Error' && exit 1
 done
+
 chmod 755 iaa.sh
-chmod 644 iaa.conf
-chmod 644 pkglist.txt
+for f in iaa.conf iaa-i3.conf iaa-kde.conf pkglist.txt pkglist-gtk.txt pkglist-qt.txt; do
+  chmod 644 $f
+done
+
+echo
 echo 'Done!'
+echo 'Edit iaa.conf and pkglist.txt (see exaples) and then run ./iaa.sh'
+echo
+
 exit 0
