86392e71b6c5fbceccd710cbfaea05a0884ef5bb
wachambo <wchmb@protonmail.com>
2017-12-31 15:13:25 +0100

    Improve readability of the log file
    MOD: iaa_extra.sh each function outputs with format to tty
    and without format to log

--- iaa_extra.sh
+++ iaa_extra.sh
@@ -1,32 +1,40 @@
 #!/usr/bin/env bash
 
 # Some global vars
-unset log tty
+unset log
 log='./iaa.log'
-tty=$(tty)
 
 error_conf()
 {
-  printf "\033[1;31m%s\n\033[1;0m" "Error in var configuration $1" | tee -a  $log
+  printf "\033[1;31m%s\n\033[1;0m" "Error in var configuration $1"
+  printf "%s\n" "Error in var configuration $1" >> $log
   exit 1
 }
+
 error()
 {
-  printf "\033[1;31m%s\n\033[1;0m" "Error: $@" | tee -a  $log
+  printf "\033[1;31m%s\n\033[1;0m" "Error: $@"
+  printf "%s\n" "Error: $@" >> $log
   exit 1
 }
+
 info()
 {
-  printf "%s\n" "$@" | tee $tty
+  printf "%s\n" "$@"
+  printf "%s\n" "$@" >> $log
 }
+
 alert()
 {
-  printf "\033[1;31m%s\n\033[1;0m" "$@" | tee $tty
+  printf "\033[1;31m%s\n\033[1;0m" "$@"
+  printf "%s\n" "$@" >> $log
 }
+
 run_root()
 {
   arch-chroot /mnt "$@"
 }
+
 #run_root_file()
 #{
 #  local filename=$1
